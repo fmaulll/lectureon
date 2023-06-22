697cef186c69b9864b86bfb04b8239006d7a6626
fmaulll <maul2821@gmail.com>
2023-06-22 15:50:35 +0700

    user model

---
+++ models/userModel.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Email    string `gorm:"unique" json:"email"`
+	Username string `json:"usename"`
+	Password string
+}
