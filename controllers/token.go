4ae723c6e8455382d711612bdbf88a5a0e6d2be9
fmaulll <maul2821@gmail.com>
2023-06-23 09:56:38 +0700

    make generatetoken func

---
+++ controllers/token.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"os"
+	"time"
+
+	"github.com/fmaulll/lectureon/initializers"
+	"github.com/fmaulll/lectureon/models"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GenerateToken(id uint) (map[string]string, error) {
+
+	var user *models.User
+	initializers.DB.First(&user, "id = ?", id)
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	refreshTokenString, err := refreshToken.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		return nil, err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":      user.ID,
+		"id":       user.ID,
+		"email":    user.Email,
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Minute * 15).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]string{
+		"access_token":  tokenString,
+		"refresh_token": refreshTokenString,
+	}, nil
+}
