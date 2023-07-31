280d8548218811353c3074dd21eb4e8b87d88a87
fmaulll <maul2821@gmail.com>
2023-07-31 14:03:42 +0700

    update model lecturer

--- models/lecturerModel.go
+++ models/lecturerModel.go
@@ -8,7 +8,5 @@ type Lecturer struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	Title     string    `json:"title"`
-	Url       string    `json:"url"`
-	PostId    int64     `json:"postId"`
+	StudentID int64     `json:"studentId"`
 }
